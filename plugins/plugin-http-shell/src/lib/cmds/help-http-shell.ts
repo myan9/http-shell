dab264c39093fca3145c3ecd183c1199c795f88b
Mengting Yan <mengting.yan1@ibm.com>
2020-10-21 15:49:03 -0400

    enhance help

--- plugins/plugin-http-shell/src/lib/cmds/help-http-shell.ts
+++ plugins/plugin-http-shell/src/lib/cmds/help-http-shell.ts
@@ -13,41 +13,80 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
- import { Registrar, MultiModalResponse } from "@kui-shell/core";
+​
+ import { Registrar, NavResponse } from "@kui-shell/core";
  import { toplevelUsage as usage } from '../usage'
 // import Debug from "debug";
-
+​
 // const debug = Debug("plugins/plugin-http-shell/test");
-
-const resp: MultiModalResponse = {
-    metadata: { name: "HTTP Shell Help" },
-    kind: "Top",
-    modes: [
+​
+const resp: NavResponse = {
+  apiVersion: 'kui-shell/v1',
+  kind: 'NavResponse',
+  breadcrumbs: [{ label: "Help" }, { label: "HTTP Shell" }],
+  menus: [
       {
-        mode: "Available commands",
-        content: `
-| command | description | example
-|---|---|---|
-| set auth | opens a dialog to sets the basic authentication credentials to use in further requests | > set auth |
-| reset auth | clear the basic authentication credentials | > reset auth |
-| set url <url> | sets the base url to use in further requests | > set url http://127.0.0.1:8080 |
-| get url | prints the base url | > get url |
-| get <uri> | executes the GET request to url=<base-url>+<uri> | > get /collection |
-| edit <file> | opens <file> for editing. Tip, hit key F1 for list of editor commands | > edit body.json |
-| post <uri> <file> | executes the request POST <base-url>+<uri>, sending the content of <file> as the request body | > post /collection body.json |
-| put <uri> <file> | executes the request PUT <base-url>+<uri>, sending the content of <file> as the request body | > put /collection body.json |
-| patch <uri> <file> | executes the request PATCH <base-url>+<uri>, sending the content of <file> as the request body | > patch /collection body.json |
-| delete <uri> | executes the DELETE request to url=<base-url>+<uri> | > delete /collection |
-| set header <name> <value> | set the header <name> to <value> | > set header If-Match 5f7f35efcb800f2502f95cb5 |
-| get headers | prints the current set headers | > get headers |
-| clear headers | clears the headers | > clear headers |
-`,
-        contentType: "text/markdown"
+        label: "Available commands",
+        items: [
+          { mode: 'Authentication',
+            contentType: "text/markdown",
+            content: `
+#### set auth
+Opens a dialog to sets the basic authentication credentials to use in further requests
+​
+#### reset auth
+Clear the basic authentication credentials
+`
+          },
+          { mode: 'Configuration',
+            contentType: "text/markdown",
+            content: `
+#### set url <url>
+Sets the base url to use in further requests
+> e.g. [\`set url https://postman-echo.com\`](#kuiexec?command=set%20url%20https%3A%2F%2Fpostman-echo.com)
+​
+#### [get url](#kuiexec?command=get%20url)
+Prints the base url
+​
+#### edit <file>
+Opens <file> for editing. Tip, hit key F1 for list of editor commands
+`
+          },
+          { mode: 'Access Methods',
+            contentType: "text/markdown",
+            content: `
+#### get <uri>
+Executes the GET request to url=<base-url>+<uri>
+> e.g. [\`get /get?foo1=bar1&foo2=bar2\`](#kuiexec?command=get%20%2Fget%3Ffoo1%3Dbar1%26foo2%3Dbar2)
+​
+#### post <uri> <file>
+Executes the request POST <base-url>+<uri>, sending the content of <file> as the request body
+​
+#### put <uri> <file>
+Executes the request PUT <base-url>+<uri>, sending the content of <file> as the request body
+​
+#### patch <uri> <file>
+Executes the request PATCH <base-url>+<uri>, sending the content of <file> as the request body
+​
+#### delete <uri>
+Executes the DELETE request to url=<base-url>+<uri>
+​
+#### set header <name> <value>
+Set the header <name> to <value>
+> e.g. \`set header If-Match 5f7f35efcb800f2502f95cb5\`
+​
+#### get headers
+Prints the current set headers
+​
+#### clear headers
+Clears the headers
+`
+          }
+        ]
       }
     ]
   };
-
+​
 export default async (registrar: Registrar) => {
   registrar.listen("/help/http-shell", () => resp, { usage: usage });
 };
